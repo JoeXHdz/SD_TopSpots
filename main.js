a30ca0cd7e94fd91ade1b8894300c5112c640bcf
Joel <jxhernandez6531@gmail.com>
2024-10-01 14:59:46 -0500

    SD_TopSpots

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
-// write your code here
-
-});
+    
+    $.getJSON('data.json', function(data) {
+      
+      data.forEach(function(spot) {
+        const name = spot.name;
+        const description = spot.description;
+        const latitude = spot.location[0];
+        const longitude = spot.location[1];
+  
+       
+        const mapLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        
+      
+        const row = `
+          <tr>
+            <td>${name}</td>
+            <td>${description}</td>
+            <td><a href="${mapLink}" target="_blank">Open in Google Maps</a></td>
+          </tr>
+        `;
+        
+       
+        $('#topSpotsTable').append(row);
+      });
+    });
+  });
+  
